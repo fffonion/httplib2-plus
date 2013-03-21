06ba2670265e7070ca1304e6732d9395fddfd4e8
fffonion <fffonion@gmail.com>
2013-03-21 16:29:01 +0800

    initial commit

---
+++ python2/sample.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+import httplib2,urllib
+def hooker():
+    print 'get chunk'
+h = httplib2.Http(callback_hook=hooker,chunk_size=1000)
+#h = httplib2.Http()
+resp, content = h.request("http://www.roame.net")
+print resp
+print len(content)
+import urllib22
+proxy_support = urllib22.ProxyHandler()
